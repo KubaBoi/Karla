bc97b7fb5ab4392cad1bc307ca3e51def16cbb0a
KubaBoi <jakubanderle@outlook.cz>
2022-04-14 07:52:43 +0200

    debug

--- src/tests/sendWavFileTest.py
+++
@@ -1,8 +0,0 @@
-import os
-import requests
-
-with open(os.path.join(__file__, "..", "recording2.wav"), "rb") as f:
-    data = f.read()
-
-r = requests.post("http://localhost:8004/recognition/fromWav", data=data)
-print(r.text)
